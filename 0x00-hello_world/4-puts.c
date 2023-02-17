2a2f93f89afe8f2330dd9b635c2b857c5ff84031
Ousmane BA <ousmane.ba4@uadb.edu.sn>
2023-02-17 02:33:07 +0000

    Task 4 Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,11 @@
 
 /**
 *main - Entry point
-*return: 0
+*Return: Always 0 (Success)
 */
 
-int main(void) 
-{ 
+int main(void)
+}
 	puts("\"Programming is like building a multilingual puzzle"); 
 	return (0); 
 }
