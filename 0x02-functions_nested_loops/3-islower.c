f4f40f4d2c10b912fa23df1152e820ab9c261a27
Ousmane BA <ousmane.ba4@uadb.edu.sn>
2023-02-22 04:30:59 +0000

    Task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* _islower -> checks if the character is in lowercase
+* Return 1 or 0
+*/
+
+int _islower(int c)
+{
+	int c;
+
+	if (c == 97)
+	{
+		return (1);
+	} 
+	else
+	{
+		return (0);
+	}
+}
