f0bd94e6aae88d5d87e78e580cf0ca93d3a2c5dd
Ousmane BA <ousmane.ba4@uadb.edu.sn>
2023-02-22 15:02:24 -0800

    Jack bauer s day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,37 +1,24 @@
-#include <unistd.h>
+#include "main.h"
+
 /**
-* jack_bauer - jack bauer
+* jack_bauer - Prints every minute of the day of Jack Bauer
+* starting from 00:00 to 23:59.
 */
 
 void jack_bauer(void)
 {
-    int i, j;
-    for (i = 0; i < 24; j++)
-    {
-        for (j = 0; j< 60, j++)
-                 {
-            if (i < 10)
-                {
-                _putchar('0');
-                _putchar(i + '0');
-                }
-            else if (i >= 10)
-            {
-                _putchar((i / 10) + '0');
-                _putchar((i % 10) + '0');
-            }
-            if (j < 10)
-            {
-                _putchar(';');
-                _putchar('0');
-                _putchar(j + '0');
-            }
-            else if (j > 10)
-            {
-                _putchar(':');
-                _putchar((j / 10) + '0');
-                _putchar((j % 10) + '0');
-            }
-        }
-    }
+	int hour, minute;
+
+	for (hour = 0; hour <= 23; hour++)
+	{
+		for (minute = 0; minute <= 59; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
 }
