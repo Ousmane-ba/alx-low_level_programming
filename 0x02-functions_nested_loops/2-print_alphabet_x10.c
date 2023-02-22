dc6bee3d94be4510bf902fe037bed94a91327b33
Ousmane BA <ousmane.ba4@uadb.edu.sn>
2023-02-22 02:54:37 +0000

    Alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_alphabet -> prints in lowercase the alphabet
+*/
+
+void print_alphabet_x10(void);
+{
+	int j;
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+		for (i = 'a'; j <= 'z'; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('n\');
+	}
+}
