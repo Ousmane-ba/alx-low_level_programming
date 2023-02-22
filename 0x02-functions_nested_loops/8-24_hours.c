95bb3d53e8377c483aad26aa8d483bb8ea4ed9fa
Ousmane BA <ousmane.ba4@uadb.edu.sn>
2023-02-22 09:04:20 +0000

    Task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -3,15 +3,35 @@
 * jack_bauer - jack bauer
 */
 
-void jack_bauer(void) {
-    for (int hour = 0; hour < 24; hour++) {
-        for (int minute = 0; minute < 60; minute++) {
-            _putchar((hour / 10) + '0');
-            _putchar((hour % 10) + '0');
-            _putchar(':');
-            _putchar((minute / 10) + '0');
-            _putchar((minute % 10) + '0');
-            _putchar('\n');
+void jack_bauer(void)
+{
+    int i, j;
+    for (i = 0; i < 24; j++)
+    {
+        for (j = 0; j< 60, j++)
+                 {
+            if (i < 10)
+                {
+                _putchar('0');
+                _putchar(i + '0');
+                }
+            else if (i >= 10)
+            {
+                _putchar((i / 10) + '0');
+                _putchar((i % 10) + '0');
+            }
+            if (j < 10)
+            {
+                _putchar(';');
+                _putchar('0');
+                _putchar(j + '0');
+            }
+            else if (j > 10)
+            {
+                _putchar(':');
+                _putchar((j / 10) + '0');
+                _putchar((j % 10) + '0');
+            }
         }
     }
 }
