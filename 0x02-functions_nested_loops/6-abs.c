3c5653aabfec7ce0e350f01fcc00a4fb91f75897
Ousmane BA <ousmane.ba4@uadb.edu.sn>
2023-02-22 06:36:48 +0000

    Task 6 Absolute value of a number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _abs - computes the absolute value of an integer
+* @n: integer
+* Return: return the absolute value of a number
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+		return (n);
+	}
+}
