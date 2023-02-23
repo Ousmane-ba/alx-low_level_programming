5a6e7641062054187ad295b1c5bb1408357476f6
Ousmane BA <ousmane.ba4@uadb.edu.sn>
2023-02-23 13:38:03 -0800

    Debugging Task 0 Negative or positive

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,28 +1,17 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "main.h"
+
 /**
  * main - entry point
  * Description: prints  if a number is positive or negative.
  * Return: 0
  */
+
 int main(void)
 {
-	int n;
+	int i;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else if (n < 0)
-	{
-		printf("%d is negative\n", n);
-	}
+	i = 0;
+	positive_or_negative(i);
+
 	return (0);
 }
