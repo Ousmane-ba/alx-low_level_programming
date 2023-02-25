b3de8a412f5e5e597e57901187aad6720b32da27
Ousmane BA <ousmane.ba4@uadb.edu.sn>
2023-02-24 16:08:20 -0800

    Task 4 Function that prints the numbers from 0 to 9

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
-* print_most_numbers - prints the numbers from 0 to 9 
-* Return: void
+* print_most_numbers - prints the numbers from 0 to 9
+* Return: Always 0
 */
 
 void print_most_numbers(void)
